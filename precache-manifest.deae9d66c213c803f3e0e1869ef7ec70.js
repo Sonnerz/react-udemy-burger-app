25b2d299b0e3a41d358a54dee77b56aa89ef3404
Sonnerz <Sonnerz@users.noreply.github.com>
2019-04-17 09:01:50 +0100

    Updates

---
+++ precache-manifest.deae9d66c213c803f3e0e1869ef7ec70.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "7325bbfd21a4643db762",
+    "url": "/react-udemy-burger-app/static/css/main.b43e6292.chunk.css"
+  },
+  {
+    "revision": "7325bbfd21a4643db762",
+    "url": "/react-udemy-burger-app/static/js/main.7325bbfd.chunk.js"
+  },
+  {
+    "revision": "ba2e90ba816542e42676",
+    "url": "/react-udemy-burger-app/static/css/1.94a99433.chunk.css"
+  },
+  {
+    "revision": "ba2e90ba816542e42676",
+    "url": "/react-udemy-burger-app/static/js/1.ba2e90ba.chunk.js"
+  },
+  {
+    "revision": "56038159156a0a5865c2",
+    "url": "/react-udemy-burger-app/static/css/2.5b419e2b.chunk.css"
+  },
+  {
+    "revision": "56038159156a0a5865c2",
+    "url": "/react-udemy-burger-app/static/js/2.56038159.chunk.js"
+  },
+  {
+    "revision": "cc6d1b4a10f7ed3eb359",
+    "url": "/react-udemy-burger-app/static/css/3.307ea80b.chunk.css"
+  },
+  {
+    "revision": "cc6d1b4a10f7ed3eb359",
+    "url": "/react-udemy-burger-app/static/js/3.cc6d1b4a.chunk.js"
+  },
+  {
+    "revision": "d32156cb0c1838cf30ef",
+    "url": "/react-udemy-burger-app/static/js/4.d32156cb.chunk.js"
+  },
+  {
+    "revision": "cad780e642105998e8d7",
+    "url": "/react-udemy-burger-app/static/js/runtime~main.cad780e6.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/react-udemy-burger-app/static/media/burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "0fa0dbfb5dd9aabae8412f8044d88157",
+    "url": "/react-udemy-burger-app/index.html"
+  }
+];
